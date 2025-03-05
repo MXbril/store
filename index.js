f5348c2f63d46b2784509dcfc579f7b965184c2c
Maximo Corpus <151881259+MXbril@users.noreply.github.com>
2025-03-05 11:53:16 -0500

    cambios al acceso de estilos

--- index.js
+++ index.js
@@ -11,22 +11,22 @@
 
 // console.log("El total a pagar es: " + totalApagar);
 
-const comprar = () => {
-  let totalApagar = 0;
-  for (let i = 1; i <= 3; i++) {
-    const nombre = prompt("¿Qué producto desea llevar?");
-    const unidades = Number(prompt("¿Cuántas unidades?"));
-    const precio = Number(prompt("¿Cuánto sale cada unidad?"));
-    const subtotal = unidades * precio;
-    totalApagar += subtotal;
-  }
-  console.log(totalApagar);
-  return totalApagar;
-};
+// const comprar = () => {
+//   let totalApagar = 0;
+//   for (let i = 1; i <= 3; i++) {
+//     const nombre = prompt("¿Qué producto desea llevar?");
+//     const unidades = Number(prompt("¿Cuántas unidades?"));
+//     const precio = Number(prompt("¿Cuánto sale cada unidad?"));
+//     const subtotal = unidades * precio;
+//     totalApagar += subtotal;
+//   }
+//   console.log(totalApagar);
+//   return totalApagar;
+// };
 
-const total1 = comprar();
-const total2 = comprar();
-const total3 = comprar();
-const total = total1 + total2 + total3;
+// const total1 = comprar();
+// const total2 = comprar();
+// const total3 = comprar();
+// const total = total1 + total2 + total3;
 
-console.log("El total a pagar es: "  + total)
+// console.log("El total a pagar es: "  + total)
